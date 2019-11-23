8f6987ccca4e0f9ec5fa8a9a35f6445611d3560b
DESKTOP-UVQ51RS\bluesky <1124826889@qq.com>
2019-11-23 18:06:01 +0800

    1

---
+++ fastim-common/src/main/java/com/zyblue/fastim/common/algorithm/SingleListCircle.java
@@ -0,0 +1,14 @@
+package com.zyblue.fastim.common.algorithm;
+
+/**
+ * 单链表是否有环，并且判断环的入口
+ *
+ * 思路：
+ * 1、利用快慢指针遍历链表
+ * 2、相遇后，遍历环行链表获取环形的长度K
+ * 3、2个指针重新指向头节点，快指针先走K步
+ * 4、快慢指针都只走一步
+ * 5、相遇的话就是环的入口
+ */
+public class SingleListCircle {
+}
