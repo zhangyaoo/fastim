19bb8173adf38251f530cc7be69fbf9ffd4d3c87
DESKTOP-UVQ51RS\bluesky <1124826889@qq.com>
2019-12-23 22:44:51 +0800

    算法

--- fastim-common/src/main/java/com/zyblue/fastim/common/algorithm/FindArrForOneTime.java
+++ fastim-common/src/main/java/com/zyblue/fastim/common/algorithm/FindArrForOneTime.java
@@ -10,7 +10,7 @@ package com.zyblue.fastim.common.algorithm;
  */
 public class FindArrForOneTime {
 
-    public int find(int[] arr){
+    public static int find(int[] arr){
         int val = 0;
 
         for(int i=0; i<arr.length; i++)
@@ -19,5 +19,17 @@ public class FindArrForOneTime {
         }
 
         return val;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new int[5];
+        ints[0] = 5;
+        ints[1] = 3;
+        ints[2] = 2;
+        ints[3] = 2;
+        ints[4] = 3;
+
+        int i = find(ints);
+        System.out.println("i:" + i);
     }
 }
